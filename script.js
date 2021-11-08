fa2039c7b9d5f04455ef3758017d92559ff6adb7
Thiago Pereira Mendonca <thiagomendonca09@gmail.com>
2021-11-08 17:19:24 -0400

    Add pages

--- script.js
+++ script.js
@@ -29,8 +29,5 @@ $(".slick-list").slick({
         slidesToScroll: 1,
       },
     },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ],
 });
