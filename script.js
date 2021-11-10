470583b4a805a9b6d7b53bd2319a518a34ee9988
Thiago Pereira Mendonca <thiagomendonca09@gmail.com>
2021-11-10 12:16:10 -0400

    Form validation

--- script.js
+++ script.js
@@ -31,3 +31,54 @@ $(".slick-list").slick({
     },
   ],
 });
+
+$(".form-contato").submit(function () {
+  var name = $("[name=name]");
+  var email = $("[name=email]");
+  var tel = $("[name=tel]");
+  var message = $("[name=message]");
+
+  var error = $(".alert");
+  var field = $(".field-error");
+
+  error.addClass("d-none");
+  $(".is-invalid").removeClass("is-invalid");
+
+  //VALIDATION NAME
+  if (name.val() == "") {
+    error.removeClass("d-none");
+    field.html("Nome");
+    name.focus();
+    name.addClass("is-invalid");
+    return false;
+  }
+
+  //VALIDATION EMAIL
+  if (email.val() == "") {
+    error.removeClass("d-none");
+    field.html("E-mail");
+    email.focus();
+    email.addClass("is-invalid");
+    return false;
+  }
+
+  //VALIDATION TEL
+  if (tel.val() == "") {
+    error.removeClass("d-none");
+    field.html("Telefone");
+    tel.focus();
+    tel.addClass("is-invalid");
+    return false;
+  }
+
+  //VALIDATION MESSAGE
+  if (message.val() == "") {
+    error.removeClass("d-none");
+    field.html("Mensagem");
+    message.focus();
+    message.addClass("is-invalid");
+    return false;
+  }
+
+  return true;
+});
