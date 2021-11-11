687c70cf3855cdf77968b813eb1b7dccc9477a38
Thiago Pereira Mendonca <thiagomendonca09@gmail.com>
2021-11-11 15:44:24 -0400

    Add mask tel, alt img and reCAPTCHA

--- script.js
+++ script.js
@@ -82,3 +82,16 @@ $(".form-contato").submit(function () {
 
   return true;
 });
+
+var SPMaskBehavior = function (val) {
+    return val.replace(/\D/g, "").length === 11
+      ? "(00) 00000-0000"
+      : "(00) 0000-00009";
+  },
+  spOptions = {
+    onKeyPress: function (val, e, field, options) {
+      field.mask(SPMaskBehavior.apply({}, arguments), options);
+    },
+  };
+
+$("[name=tel]").mask(SPMaskBehavior, spOptions);
